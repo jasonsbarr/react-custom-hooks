334f34ff14f716bda8fe66f919282df0342a91bc
Jason Barr <jason@jasonsbarr.com>
2019-10-15 16:19:22 -0500

    Add matchAsyncState helper

---
+++ src/utils/matchAsyncState.js
@@ -0,0 +1,17 @@
+import { getNull } from "./";
+
+/**
+ * Renders element based on a Promise's current status
+ *
+ * @param {Object} state - component's local state with Promise status
+ */
+
+const matchAsyncState = state => match =>
+  // prettier-ignore
+  state.initial ? match.initial()
+    : state.pending ? match.pending()
+    : state.error   ? match.error(state.error)
+    : state.data    ? match.data(state.data)
+    : getNull();
+
+export default matchAsyncState;
